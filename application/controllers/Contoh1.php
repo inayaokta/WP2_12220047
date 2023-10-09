0cd9b0c6005b464b3e3039967fb6c3ff469835d8
inayaokta <inayaoktavianadh@gmail.com>
2023-10-09 20:32:49 +0700

    pertemuan-3

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,12 +1,11 @@
 <?php
-class Contoh1 extends CI_Controller
+class Contoh1 extends CI_Controller {
+public function index()
 {
-    public function index()
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Inaya oktaviana Dayuning Hapsari
-            saya tinggal di daerah Sleman
-            olah raga yang saya sukai adalah
-            Bersepeda";
-    }
+    echo "<h1>Perkenalkan</h1>";
+    echo " Nama saya Inaya Oktaviana Dayuning Hapsari <br>
+    saya tinggal di daerah Sleman <br>
+    olahraga yang saya sukai adalah Bersepeda";
+
+}
 }
